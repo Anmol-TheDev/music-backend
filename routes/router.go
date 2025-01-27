2817ec31da14d1073da95e80e5c72078bd37aeeb
Chandrashekhar Kachawa <42296139+mcctrix@users.noreply.github.com>
2025-01-27 13:36:35 +0530

    added indent in router.go

--- routes/router.go
+++ routes/router.go
@@ -7,13 +7,13 @@ import (
 )
 
 func Router() {
-	http.HandleFunc("/getsptfyplaylist", handler.HandlePlaylist)  // id = spotify id / return tracks inside spotify playlist | sptfy = spotify
-http.HandleFunc("/fchytsong", handler.FromYouTube)  // fetching song that is not available on jio savan 
-	http.HandleFunc("/srchbar", handler.HandleSearch) // end point for api debounceing 
+	http.HandleFunc("/getsptfyplaylist", handler.HandlePlaylist)     // id = spotify id / return tracks inside spotify playlist | sptfy = spotify
+	http.HandleFunc("/fchytsong", handler.FromYouTube)               // fetching song that is not available on jio savan
+	http.HandleFunc("/srchbar", handler.HandleSearch)                // end point for api debounceing
 	http.HandleFunc("/homesuggestion", handler.HnadleHomeSuggestion) // home suggestion like tracks and playlist
-	http.HandleFunc("/song",handler.Song)
-	http.HandleFunc("/auth/register", auth.Register) // auth register user
-	http.HandleFunc("/auth/login", auth.Login) // auth login user
-	http.HandleFunc("/auth/tokencheck",auth.CheckToken) // check user is authorized 
-	http.HandleFunc("/auth/logout",auth.Logout) // user logout 
+	http.HandleFunc("/song", handler.Song)
+	http.HandleFunc("/auth/register", auth.Register)     // auth register user
+	http.HandleFunc("/auth/login", auth.Login)           // auth login user
+	http.HandleFunc("/auth/tokencheck", auth.CheckToken) // check user is authorized
+	http.HandleFunc("/auth/logout", auth.Logout)         // user logout
 }
